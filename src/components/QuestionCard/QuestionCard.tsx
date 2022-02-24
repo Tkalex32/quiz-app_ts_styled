aa7eff6814df90717b18843d2a518335ffe9e3a7
Tkalex32 <tkalexsc@gmail.com>
2022-02-24 20:43:34 +0100

    feat:  finalize app w/o styling

---
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -0,0 +1,37 @@
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers.map((answer, i) => (
+          <div key={i}>
+            <button disabled={userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
